11c85b9557b95a5223bc29461d0d4b439d19c8b0
JairDesktop <jair3202@gmail.com>
2018-11-16 21:00:48 -0500

    Fixes

---
+++ .vscode/cquery_cached_index/c@@users@msi@desktop@tp clone@tp-2018-19/include@functions.h
@@ -0,0 +1,38 @@
+#ifndef FUNCTIONS_H
+#define  FUNCTIONS_H
+
+#include "api.h"
+using namespace pros;
+
+extern Controller master;
+extern Controller partner;
+
+extern Motor left_back;
+extern Motor right_back;
+extern Motor left_front;
+extern Motor right_front;
+
+extern Motor left_lift;
+extern Motor right_lift;
+
+extern Motor flywheel_mtr;
+
+extern Motor intake_mtr;
+
+extern Motor cap_flip_mtr;
+
+void driveSpeed(double left, double right, int side);
+
+void driveDist(float dist, int speed); // in inches, linear distnance
+
+void driveTurn(double degrees, int speed);
+
+void liftSpeed(int speed);
+
+void liftSet(int pos, int speed);
+
+void flyWheelSet(int velocity);
+
+void capFlip();
+
+#endif
